fbe4061462890ebbb1ca3f7a2c88b26672ecc37a
DatLag <46448715+DATL4G@users.noreply.github.com>
2020-10-02 18:47:34 +0200

    code cleanup, added stacktrace option for more info

--- app/src/test/java/de/datlag/openfe/OtherUnitTest.kt
+++ app/src/test/java/de/datlag/openfe/OtherUnitTest.kt
@@ -1,22 +1,20 @@
 package de.datlag.openfe
 
 import de.datlag.openfe.util.toHumanReadable
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 class OtherUnitTest {
 
     @Test
     fun humanReadableByteSize() {
         val size: Long = 2048
+        val biByteActual: String = "2,0 KiB"
+        val nonBiByteActual: String = "2,0 kB"
 
-        assertEquals(size.toHumanReadable(true), "2,0 KiB")
-        assertEquals(size.toHumanReadable(false), "2,0 kB")
+        assertEquals(size.toHumanReadable(true), biByteActual)
+        assertEquals(size.toHumanReadable(false), nonBiByteActual)
     }
 
 }
